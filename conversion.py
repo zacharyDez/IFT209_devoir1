06fad1c160b50562d45e9b47e9cd1d3a1c72c861
zacharyDez <zachary.deziel@usherbrooke.ca>
2020-01-20 13:59:17 -0500

    separated conversion_latex from conversion

--- conversion.py
+++ conversion.py
@@ -1,6 +1,3 @@
-import math
-
-
 def number(val: int) -> str:
     """
     returns number associated with numerical value
@@ -24,31 +21,6 @@ def convert_from_base_10(x: int, b: int) -> str:
     return y
 
 
-def convert_from_base_10_latex(x: int, b: int) -> str:
-    """
-    returns string in latex format for math demonstration
-    """
-
-    msg = "y = x \% b\qquad x = x\n\n"
-    msg += f"y = {x} \% {b} \qquad x = {x}\n\n"
-    y = ""
-
-    # prints empty string if y entered
-    msg += f"y = {x} \% {b}\qquad x = {x}\n\n"
-
-    while True:
-        y = number(x % b) + y
-        x = x // b
-        msg += f"y = {x} \% {b} + {y}\qquad "
-        msg += f"x = {x}//{b}\n\n"
-
-        if x == 0:
-            msg += f"y = {y}\qquad \qquad x = {x}\n\n"
-            break
-
-    return msg
-
-
 def convert_to_base_10(x: str, b: int, i=0) -> int:
     """
     returns number converted from base b to base 10
@@ -63,30 +35,7 @@ def convert_to_base_10(x: str, b: int, i=0) -> int:
     return int(x[-(i + 1)]) + b * (convert_to_base_10(x, b, i + 1))
 
 
-def convert_to_base_10_latex(x: str, b: int) -> str:
-    """
-    returns string in latex format for math demonstration
-    """
-
-    msg = f"{x}_{{{b}}} = y_{{10}}\n\n"
-    msg += f"x_0 + b \\times (x_1 + b \\times (... + x_{{n-1}}\\times b))\n\n"
-
-    # allow function to be used with numbers
-    if not isinstance(x, str):
-        x = str(x)
-
-    for i in range(0, len(x)):
-        if i == len(x) - 1:
-            msg += f"{x[-(i + 1)]} + {b}{')' * i}"
-        else:
-            msg += f"{x[-(i + 1)]} + {b} \\times ("
-
-    y = convert_to_base_10(x, b)
-
-    msg += f"\n\n{x}_{{{b}}} = {y}_{{10}}"
-    return msg
-
-
 # example
 if __name__ == "__main__":
-    pass
+    x = "5FB1C7"
+    print(convert_to_base_10(x, 16))
---
+++ conversion_latex.py
@@ -0,0 +1,51 @@
+from conversion import convert_to_base_10
+import math
+
+
+def convert_to_base_10_latex(x: str, b: int) -> str:
+    """
+    returns string in latex format for math demonstration
+    """
+
+    msg = f"{x}_{{{b}}} = y_{{10}}\n\n"
+    msg += f"x_0 + b \\times (x_1 + b \\times (... + x_{{n-1}}\\times b))\n\n"
+
+    # allow function to be used with numbers
+    if not isinstance(x, str):
+        x = str(x)
+
+    for i in range(0, len(x)):
+        if i == len(x) - 1:
+            msg += f"{x[-(i + 1)]} + {b}{')' * i}"
+        else:
+            msg += f"{x[-(i + 1)]} + {b} \\times ("
+
+    y = convert_to_base_10(x, b)
+
+    msg += f"\n\n{x}_{{{b}}} = {y}_{{10}}"
+    return msg
+
+
+def convert_from_base_10_latex(x: int, b: int) -> str:
+    """
+    returns string in latex format for math demonstration
+    """
+
+    msg = "y = x \% b\qquad x = x\n\n"
+    msg += f"y = {x} \% {b} \qquad x = {x}\n\n"
+    y = ""
+
+    # prints empty string if y entered
+    msg += f"y = {x} \% {b}\qquad x = {x}\n\n"
+
+    while True:
+        y = math.number(x % b) + y
+        x = x // b
+        msg += f"y = {x} \% {b} + {y}\qquad "
+        msg += f"x = {x}//{b}\n\n"
+
+        if x == 0:
+            msg += f"y = {y}\qquad \qquad x = {x}\n\n"
+            break
+
+    return msg
